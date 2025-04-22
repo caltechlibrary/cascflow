5ae0e887e54ca740df0ac1e8aa318c552663c598
Tommy Keswick <t4k@users.noreply.github.com>
2025-04-22 12:31:09 -0700

    ⚗️ test a function

--- src/cascflow/cascflow.py
+++ src/cascflow/cascflow.py
@@ -1,3 +1,7 @@
 def validate(arg1, arg2):
     print("hello from cascflow validate()!")
     print(f"arg1: {arg1}, arg2: {arg2}")
+
+def execute(arg1, arg2):
+    print("hello from cascflow run()!")
+    print(f"arg1: {arg1}, arg2: {arg2}")
